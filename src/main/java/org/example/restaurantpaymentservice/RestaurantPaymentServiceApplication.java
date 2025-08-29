572b8740243899c2d299892ff82d33d3eef37733
EliasEgel <elias.egelrud@gmail.com>
2025-08-29 15:02:35 +0200

    Initial commit: Spring Boot project

---
+++ src/main/java/org/example/restaurantpaymentservice/RestaurantPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.restaurantpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantPaymentServiceApplication.class, args);
+    }
+
+}
