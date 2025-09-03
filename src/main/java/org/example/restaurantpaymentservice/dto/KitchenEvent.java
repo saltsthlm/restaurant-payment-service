3cbeb50a7e2474dd630fcadba12316967293b593
David Aslan <davidaslan112671@live.se>
2025-09-03 13:42:11 +0200

    added Kitchen Event dto and mapped inMessage to it on kafka listen

---
+++ src/main/java/org/example/restaurantpaymentservice/dto/KitchenEvent.java
@@ -0,0 +1,33 @@
+package org.example.restaurantpaymentservice.dto;
+
+import lombok.Builder;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Builder
+public record KitchenEvent(
+        UUID eventId,
+        UUID ticketId,
+        UUID orderId,
+        Status status,   // CANCELED
+        Stage stage,     // PENDING | ACCEPTED | IN_PROGRESS | READY
+        Reason reason,   // CLOSED | CAPACITY | ORDER_CANCELED | PAYMENT_FAILED | OPERATOR
+        Instant occurredAt)    // 2025-09-01T12:34:56Z
+{
+    public enum Status {
+        CANCELED
+    }
+
+    public enum Stage {
+        PENDING, ACCEPTED, IN_PROGRESS, READY
+    }
+
+    public enum Reason {
+        CLOSED, CAPACITY, ORDER_CANCELED, PAYMENT_FAILED, OPERATOR
+    }
+
+
+}
+
+
--- src/main/java/org/example/restaurantpaymentservice/service/KafkaConsumerService.java
+++ src/main/java/org/example/restaurantpaymentservice/service/KafkaConsumerService.java
@@ -1,15 +1,25 @@
 package org.example.restaurantpaymentservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.restaurantpaymentservice.dto.KitchenEvent;
 import org.springframework.kafka.annotation.KafkaListener;
 
 public class KafkaConsumerService {
+    ObjectMapper mapper;
+
+    public KafkaConsumerService(ObjectMapper mapper) {
+        this.mapper = new ObjectMapper();
+    }
 
     @KafkaListener(id = "kitchen", topics = "kitchen.prepared")
-    void listen(String inMessage){
+    void listen(String inMessage) throws JsonProcessingException {
         ///read message
         System.out.println("Kafka kitchen message: "+inMessage);
         //convert message to some sort of dto
+        KitchenEvent event = mapper.readValue(inMessage, KitchenEvent.class);
         //validate message data
+
         //if bad reject message
         //else refund payment
 
