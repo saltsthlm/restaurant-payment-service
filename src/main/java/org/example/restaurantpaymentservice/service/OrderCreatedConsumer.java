d8c6b53e3b89bbd4760d4c8770e6e75a66e3a057
Helloinot <37874965+Helloinoit@users.noreply.github.com>
2025-09-11 11:23:14 +0200

    updated reading of Order event

--- src/main/java/org/example/restaurantpaymentservice/service/OrderCreatedConsumer.java
+++ src/main/java/org/example/restaurantpaymentservice/service/OrderCreatedConsumer.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.example.restaurantpaymentservice.evenDto.OrderMessageDto;
 import org.example.restaurantpaymentservice.idempotent.ConsumedEvent;
 import org.example.restaurantpaymentservice.idempotent.ConsumedEventRepository;
@@ -26,26 +27,31 @@ public class OrderCreatedConsumer {
     private final ConsumedEventRepository consumedEventRepository;
 
     @Transactional
-    @KafkaListener(topics = "order.created")
-    void listen(String inMessage) {
+    @KafkaListener(topics = "order.created.v1")
+    void listen(ConsumerRecord<String, String> record) {
         try {
+            log.info("Received from topic={}, partition={}, offset={}",
+                    record.topic(), record.partition(), record.offset());
+
+            String json = record.value();
             ObjectReader reader = mapper
                     .readerFor(OrderMessageDto.class)
                     .without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
-            OrderMessageDto order = reader.readValue(inMessage);
+            OrderMessageDto order = reader.readValue(json);
+
             if (consumedEventRepository.existsById(order.eventId())) {
                 log.info("Duplicate event detected, skipping.");
                 return;
             }
+
             ConsumedEvent consumedEvent = new ConsumedEvent(order.eventId(), Instant.now());
             consumedEventRepository.save(consumedEvent);
 
             paymentService.createPendingFromOrder(order);
 
-
         } catch (JsonProcessingException e) {
-            log.error("Failed to parse order.created payload. Message: {}\nError: {}", inMessage, e.getMessage(), e);
+            log.error("Failed to parse order.created payload. Message: {}\nError: {}", record, e.getMessage(), e);
         }
 
     }
